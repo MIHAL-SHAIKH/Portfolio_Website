cedda9d1e154e93840d756e3762dc53c3f420d6f
Mihal Shaikh <mehal.shaikh@outlook.com>
2024-10-11 12:22:43 +0530

    added: tooltip with logic!

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
-
-
-
-$(document).ready(function() {
-  $("a.nav-link").on('click', function() {
-    $("a.nav-link").removeClass('active');
-    $(this).addClass('active');
+$(document).ready(function () {
+  $("a.nav-link").on("click", function () {
+    $("a.nav-link").removeClass("active");
+    $(this).addClass("active");
   });
-  
+
+
+  $(document).on('click',function() {
+    if (!event.target.classList.contains('copy-link__inner')) {
+      $('.side__icon').removeClass('active');
+  }
+  })
+
+  // $(".copy-link__inner").on("click", function(){
+  //   this.textContent = 'Copied! '
+  //   console.log('this',this)
+  // })
+
+
+  $(".copy-link__inner").on("click", function() {
+ 
+    // Copy the link to the clipboard
+    const cleanLink = $(this).closest('li').find('.side__icon').data('link');
+
+    // Copy the clean link to the clipboard
+    navigator.clipboard.writeText(cleanLink).then(function() {
+        console.log('Link copied to clipboard:', cleanLink);
+    }).catch(function(err) {
+        console.error('Error copying to clipboard:', err);
+    });
+// coppying link
+
+    const originalHTML = $(this).html();
+    $(this).html('Copied! <i class="fa-solid fa-check"></i>');
+    $(this).addClass('clicked')
+    
+   setTimeout(() => {
+     $('.side__icon').removeClass('active')
+   },1700)
+    setTimeout(() => {
+        $(this).html(originalHTML);
+        $(this).removeClass("clicked")
+    }, 2000);
+});
 });
 var swiper = new Swiper(".mySwiper", {
   // slidesPerView: 4,
   spaceBetween: 0,
-  loop:true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
     640: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     1024: {
-      slidesPerView: 4
+      slidesPerView: 4,
     },
   },
 });
@@ -36,19 +71,35 @@ var swiper = new Swiper(".mySwiper", {
 // console.warn(sections,'sections')
 
 // sectionsArray.forEach(section => {
-  //     setInterval(()=>{
-    //         section.classList.toggle('active-section');
-    //         section.classList.toggle('deactive-section')
-    //     },3000)
-    //     console.log(section)
-    // });
-    const myText = new AutoTyping({
-    
-      id: 'profesion',
-      
-      typeText: ['Programmer','Devloper','Astronaut','Plumber','Doctor','bla bla bla'],
-      textColor: 'white',
-      cursorColor:'white',
+//     setInterval(()=>{
+//         section.classList.toggle('active-section');
+//         section.classList.toggle('deactive-section')
+//     },3000)
+//     console.log(section)
+// });
+const myText = new AutoTyping({
+  id: "profesion",
 
-      
-      }).init();
+  typeText: [
+    "Programmer",
+    "Devloper",
+    "Astronaut",
+    "Plumber",
+    "Doctor",
+    "bla bla bla",
+  ],
+  textColor: "white",
+  cursorColor: "white",
+}).init();
+
+
+
+function toggleActive(el){
+  const tags = document.querySelectorAll('.side__icon');
+
+  tags.forEach( tag => {
+    tag.classList.remove('active')
+  })
+
+  el.classList.toggle('active')
+}
