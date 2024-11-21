80e5a036fe6ba19d62929269356fe8963685846f
Mihal Shaikh <mehal.shaikh@outlook.com>
2024-11-21 21:33:26 +0530

    fixed: small bugs in css

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 $(document).ready(function () {
+  const mediaQuery = window.matchMedia('(max-width: 992px)');
+
+  if(mediaQuery.matches){
+    console.warn('logged',mediaQuery)
+    alert(mediaQuery)
+    $(document).on('click',function() {
+      if (!event.target.classList.contains('copy-link__inner')) {
+        $('.side__icon').removeClass('active');
+    }
+    })
+  
+    // $(".copy-link__inner").on("click", function(){
+    //   this.textContent = 'Copied! '
+    //   console.log('this',this)
+    // })
+  
+  
+    $(".copy-link__inner").on("click", function() {
+   
+      // Copy the link to the clipboard
+      const cleanLink = $(this).closest('li').find('.side__icon').data('link');
+  
+      // Copy the clean link to the clipboard
+      navigator.clipboard.writeText(cleanLink).then(function() {
+          console.log('Link copied to clipboard:', cleanLink);
+      }).catch(function(err) {
+          console.error('Error copying to clipboard:', err);
+      });
+  // coppying link
+  
+      const originalHTML = $(this).html();
+      $(this).html('Copied! <i class="fa-solid fa-check"></i>');
+      $(this).addClass('clicked')
+      
+     setTimeout(() => {
+       $('.side__icon').removeClass('active')
+     },1700)
+      setTimeout(() => {
+          $(this).html(originalHTML);
+          $(this).removeClass("clicked")
+      }, 2000);
+  });
+  }
   $("a.nav-link").on("click", function () {
     $("a.nav-link").removeClass("active");
     $(this).addClass("active");
   });
 
 
-  $(document).on('click',function() {
-    if (!event.target.classList.contains('copy-link__inner')) {
-      $('.side__icon').removeClass('active');
-  }
+
+
+  $('.nav-item').on('click',function(){
+    const offcanvasEl = $('.offcanvas').get(0);
+    const offcanvasIns = bootstrap.Offcanvas.getInstance(offcanvasEl)
+    console.log(offcanvasIns);
+    if (offcanvasIns){
+      offcanvasIns.hide();
+      console.log('done!');
+    }
   })
-
-  // $(".copy-link__inner").on("click", function(){
-  //   this.textContent = 'Copied! '
-  //   console.log('this',this)
-  // })
-
-
-  $(".copy-link__inner").on("click", function() {
- 
-    // Copy the link to the clipboard
-    const cleanLink = $(this).closest('li').find('.side__icon').data('link');
-
-    // Copy the clean link to the clipboard
-    navigator.clipboard.writeText(cleanLink).then(function() {
-        console.log('Link copied to clipboard:', cleanLink);
-    }).catch(function(err) {
-        console.error('Error copying to clipboard:', err);
-    });
-// coppying link
-
-    const originalHTML = $(this).html();
-    $(this).html('Copied! <i class="fa-solid fa-check"></i>');
-    $(this).addClass('clicked')
-    
-   setTimeout(() => {
-     $('.side__icon').removeClass('active')
-   },1700)
-    setTimeout(() => {
-        $(this).html(originalHTML);
-        $(this).removeClass("clicked")
-    }, 2000);
-});
 });
 var swiper = new Swiper(".mySwiper", {
   // slidesPerView: 4,
@@ -91,7 +108,6 @@ const myText = new AutoTyping({
   textColor: "white",
   cursorColor: "white",
 }).init();
-
 
 
 function toggleActive(el){
